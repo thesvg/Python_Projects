bae0ddb7866e1d5cbf7c62f275c105c6504f04a2
Srividyaa Ganesh <srividyaaganesh@Srividyaas-MacBook-Pro.local>
2021-07-08 11:31:42 -0700

    last exercise for day 1 and day 1 project: Band Name Generator

---
+++ Exercise 3.py
@@ -0,0 +1,9 @@
+a = input("input a:")
+b = input("input b:")
+#the goal is to switch a and b values
+c = a
+a = b
+b = c
+#now print a and b
+print ("the value of a: " + a)
+print ("the value of b: "+ b)
---
+++ day1_project.py
@@ -0,0 +1,9 @@
+#greeting for the program
+print("Welcome to your Band Name Generator\n")
+#asking the user for the city they grew up in
+city = input("which city did you grow up in?\n")
+#ask the user for the name of their pet
+pet = input("what is the name your first pet?\n")
+#oombine the name of the city and pet to show their band name
+band = city + " "+ pet
+print("The Name of your Band is: "+ band)
