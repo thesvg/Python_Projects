abb08c7fed021ed460ca9f50ef5754412b8b8ef0
Srividyaa Ganesh <srividyaaganesh@Srividyaas-MacBook-Pro.local>
2021-07-07 20:28:56 -0700

    some exercises from 100 days of code

---
+++ Exercise 2.py
@@ -0,0 +1,8 @@
+name = input("what is your name? ")
+print("your name is" + name)
+print("the number of characters in your name is:") 
+print(len(name))
+
+#or you could also do the below
+
+print(len(input("what id your name? ")))
